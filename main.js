e4e437290e467783dd324972cfc44502f7ce8dcc
rominaportilla <rominaportilla10@gmail.com>
2023-05-06 01:27:46 -0300

    Arreglando pequeños bugs del primer desafío

--- main.js
+++ main.js
@@ -1,31 +1,35 @@
-// @ts-check
 class ProductManager{
     constructor(){
         this.products = [];
     }
+
+    static id = 0;
+
     getProducts(){
         console.log(this.products);
         return this.products
     }
-    #generateId(){
-        const id = Math.random().toString(30).substring(2);
-        return id
+
+    searchProductById(id){
+        return this.products.find((product)=> product.id == id);
     }
+
     getProductById(id){
-        const searchProduct = this.products.find((value)=> value.id == id);
-        if (searchProduct) {
-            console.log(searchProduct);
-            return searchProduct;
-        } else {
-            console.log('Not Found');
-            return undefined;
-        }
-    };
-    #verificateCode(code){
-        const searchCode = this.products.map((value)=> value.code == code);
-        return searchCode;
+        this.searchProductById(id) ? console.log(this.searchProductById(id)) : console.log('Not Found');
     }
+
+    #verificateCode(code){
+        for (let i = 0; i < this.products.length; i++){
+            if (this.products[i].code == code){
+                console.log(`The code ${code} has already been used.`);
+                break;
+            }
+        }
+    }
+
     addProduct(title, description, price, thumbnail, code, quotas){
+        this.#verificateCode(code);
+        ProductManager.id++;
         let newProduct = {
             title: title,
             description: description,
@@ -33,9 +37,9 @@ class ProductManager{
             thumbnail: thumbnail,
             code: code,
             quotas: quotas,
-            id: this.#generateId()
+            id: ProductManager.id
         }
-        if(Object.values(newProduct).some((values) => values == null || values == undefined || values == '' || this.#verificateCode(values.code))){
+        if(Object.values(newProduct).some((values) => values == null || values == undefined || values == '')){
             console.log('Error! Fields cannot be empty or repeated.')
         }else{
             this.products = [...this.products, newProduct];
@@ -46,7 +50,10 @@ class ProductManager{
 const paquete = new ProductManager();
 paquete.getProducts();
 paquete.addProduct('nueva york', 'La ciudad que nunca duerme', 1200, 'image', 'abc123', 7);
-paquete.addProduct('paris', 'La ciudad de la moda y el amor', 1500, 'image2', 'abc1234', 9);
-paquete.addProduct('tokio', 'La ciudad más grande del mundo', 1700, 'image3', 'abc134', 11);
-paquete.addProduct('sydney', 'La ciudad más linda de Australia', 2000, 'image4', 'abc124', 12);
+paquete.addProduct('paris', 'La ciudad de la moda y el amor', 1500, 'image2', 'abc124', 9);
+paquete.addProduct('tokio', 'La ciudad más grande del mundo', 1700, 'image3', 'abc125', 11);
+paquete.addProduct('sydney', 'La ciudad más linda de Australia', 2000, 'image4', 'abc126', 12);
 paquete.getProducts();
+paquete.getProductById(3);
+paquete.addProduct('sydney', 'La ciudad más linda de Australia', 2000, 'image4', 'abc126', 12);
+paquete.addProduct('cancún', '', 1500, 'image5', 'abc1235', 9)
